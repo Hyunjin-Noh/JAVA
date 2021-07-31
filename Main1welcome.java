9804bd2e165e5d7daf2b0439f8876a1a90f4fa48
Hyunjin-Noh <88017666+Hyunjin-Noh@users.noreply.github.com>
2021-07-31 10:32:56 +0900

    Add files via upload

---
+++ Main1welcome.java
@@ -0,0 +1,7 @@
+public class Main1welcome {
+  public static void main(String args[])
+  {
+	  System.out.println("Welcome!!");
+	  System.out.println("Welcome to Java world.");
+  }
+}
---
+++ Main2sorry.java
@@ -0,0 +1,7 @@
+public class Main2sorry {
+  public static void main(String args[])
+  {
+    System.out.println("Sorry~~");
+    System.out.println("I'm sorry that it is so fun~~");
+  }
+}
